a9f272c055ea77c8b3b6f76242b0ea6c7b0c5b74
harishz13 <120113377+harishz13@users.noreply.github.com>
2023-01-17 16:35:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your code here
+		let evaluatedText = document.getElementById("evaluatedText");
+        let letterCount = document.getElementById("letterCount");
+
+        evaluatedText.addEventListener("input",calculateLength)
+
+        function calculateLength(){
+        let word = evaluatedText.value;
+        let wordlength = word.length;
+
+        letterCount.innerHTML = word.length;
